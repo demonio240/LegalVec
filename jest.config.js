4aa73fac9d95ff6e96da22e05d01d41da1623882
demonio240 <ramirea126y6@gmail.com>
2026-05-07 16:22:45 -0600

    chore: add test:unit script for Jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@DocumentProcessing/(.*)$': '<rootDir>/src/DocumentProcessing/$1',
+    '^@Shared/(.*)$': '<rootDir>/src/Shared/$1'
+  }
+};
