ddf5a664b86b10f13ab8237cae149ce3e1826486
demonio240 <ramirea126y6@gmail.com>
2026-05-08 14:48:42 -0600

    Test: Update ElementMother to use Value Objects

--- test/DocumentProcessing/Vectorizer/Domain/ElementMother.ts
+++ test/DocumentProcessing/Vectorizer/Domain/ElementMother.ts
@@ -1,18 +1,28 @@
 import { Element } from "@DocumentProcessing/Vectorizer/Domain/Element";
+import { ElementId } from "@DocumentProcessing/Vectorizer/Domain/VO/ElementId";
+import { Image } from "@DocumentProcessing/Vectorizer/Domain/VO/Image";
+import { OptimizedSvg } from "@DocumentProcessing/Vectorizer/Domain/VO/OptimizedSvg";
+import { ElementPrecision } from "@DocumentProcessing/Vectorizer/Domain/VO/Precision";
+import { ReductionRate } from "@DocumentProcessing/Vectorizer/Domain/VO/ReductionRate";
+import { ElementScale } from "@DocumentProcessing/Vectorizer/Domain/VO/Scale";
 
 export class ElementMother {
     static create(params?: {
-        id?: string;
-        image?: string;
-        scale?: number;
-        precision?: number;
+        id?: ElementId;
+        image?: Image;
+        scale?: ElementScale;
+        precision?: ElementPrecision;
+        optimizedSvg?: OptimizedSvg;
+        reductionRate?: ReductionRate;
     }): Element {
-        return Element.create({
-            id: params?.id ?? "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-            image: params?.image ?? "base64string",
-            scale: params?.scale ?? 1,
-            precision: params?.precision ?? 50
-        });
+        return Element.create(
+            params?.id ?? new ElementId("f47ac10b-58cc-4372-a567-0e02b2c3d479"),
+            params?.image ?? new Image("base64string"),
+            params?.scale ?? new ElementScale(1),
+            params?.precision ?? new ElementPrecision(50),
+            params?.optimizedSvg ?? new OptimizedSvg("<svg></svg>"),
+            params?.reductionRate ?? new ReductionRate(0.5)
+        );
     }
 
 }
